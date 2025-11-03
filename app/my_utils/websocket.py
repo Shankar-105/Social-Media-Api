c8baacd488db8374e9f6adf4d623a26031c086ca
Shankar-105 <shankarbhavani862@gmail.com>
2025-11-03 16:18:50 +0530

    added comments of how chat system works for better understanding

--- app/my_utils/websocket.py
+++ app/my_utils/websocket.py
@@ -1,28 +1,45 @@
 from fastapi import WebSocket
 from typing import List, Dict
 import json
-
+# a connection manager class for managing any websocket request
+# from any user rather writing each for every guy
 class ConnectionManager:
     def __init__(self):
+        # creates a dictionary of type {int,WebSocket}
+        # to map a user to his given webSocket for live chat
         # {user_id: websocket}
-        self.active_connections: Dict[int, WebSocket] = {}
-
+        self.active_connections:Dict[int, WebSocket]={}
+    # this is the method in which we implement the connection logic of a websocket
     async def connect(self, user_id: int, websocket: WebSocket):
+        # this is where the http request is upgraded to a websocket protocol
+        # it takes a 1 or 2 secs for the handshake process so the server shouldn't 
+        # freeze here rather it need to serve other requests too so we use the await here
         await websocket.accept()
+        # after the upgrade we put the current user into our active connections dict
         self.active_connections[user_id] = websocket
+        # for debugging purposes
         print(f"User {user_id} connected via WebSocket")
-
+    # whenever the user closes the tab or
+    # the server crashes or
+    # we manually disconnect the user only then this disconnect is hit
     def disconnect(self, user_id: int):
         if user_id in self.active_connections:
             del self.active_connections[user_id]
             print(f"User {user_id} disconnected")
-
-    async def send_personal_message(self, message:dict,user_id: int):
+    # we use this method to send a json to the sender to 
+    # let him know that the message is sent without any errors
+    # and can be used further to add more features like typing,status etc
+    async def send_personal_message(self,message:dict,user_id: int):
         if user_id in self.active_connections:
+            # json.dumps() in python converts back a dict to json 
             await self.active_connections[user_id].send_text(json.dumps(message))
-
+    # we use this method to send the message from the sender to the reciver 
+    # we usually send a string here in development
+    # later we can send a json to the reciver id needed
     async def send_to_user(self, message: str, receiver_id: int):
     # Send plain text to receiver
      if receiver_id in self.active_connections:
         await self.active_connections[receiver_id].send_text(message)
+# the instance of the class which 
+# manages all users websocket requests
 manager = ConnectionManager()
--- app/routes/chat.py
+++ app/routes/chat.py
@@ -13,26 +13,46 @@ async def websocket_endpoint(
     token: str=Query(None,description="Search query params"),
     db: Session=Depends(db.getDb)
 ):
-    # Validate JWT from query param (or header later)
+    # we check whether the token is sent or not 
+    # if not sent then we close the socket connection 
+    # because we require token to validate or authunicate the user
     if not token:
         await websocket.close(code=1008)
         return
     try:
+        # get the user from the token passed token
         current_user = oauth2.getCurrentUser(token,db)
+        # if he is a different guy well then close the socket connection
+        # this prevents spoofing other users acccessing or pretending to be the actual user
         if current_user.id != user_id:
             await websocket.close(code=1008)
             return
     except:
         await websocket.close(code=1008)
         return
-     
-    await manager.connect(user_id, websocket)
+    # well if the token has passed all the above tests
+    # make a connection request by using the manager obj 
+    await manager.connect(user_id,websocket)
     try:
+        # after connecting the user successfully
+        # enter him into the live chat the while True:
+        # which stays forever until any of ther three disconnect cases are hit
         while True:
+            # the sender may send data at any time
+            # so waiting for the data until he sends is ridiculous
+            # so we use await which allows the server to do other unfinished jobs
             data = await websocket.receive_text()
+            # if msg is sent by the sender well its time 
+            # to send the msg to the rexciver and store it in db 
+            # but wait you cannot directly send that msg from 
+            # the frontend or the sender because the sender's JS sends the data 
+            # in the form of JSON so as we need to store that messsage in db 
+            # and also send a string to the reciver there is a neeed to convert 
+            # that json data to a python dict son.loads() does that for us 
             message_data = json.loads(data)
             print("data successfully loaded")
             # Save to DB
+            # store it in db so create a models.Message obj
             msg = models.Message(
                 content=message_data["content"],
                 sender_id=user_id,
@@ -43,8 +63,12 @@ async def websocket_endpoint(
             db.refresh(msg)
             print("added to db")
             # Send to receiver if online
-            await manager.send_to_user(f"User {user_id}: {msg.content}", msg.receiver_id)
+            # now send a string to reciver call the send_to_user method
+            await manager.send_to_user(f"User {user_id}: {msg.content}",msg.receiver_id)
             print("entered into message sender method via send_to_user")
+            # collect the response data that is the sender data so that 
+            # we use this for future enhancements to let the sender know
+            # much more about the status of the msg that he sent
             response_data = {
                 "id": msg.id,
                 "content": msg.content,
@@ -52,10 +76,14 @@ async def websocket_endpoint(
                 "receiver_id": msg.receiver_id,
                 "timestamp": msg.created_at.isoformat()  # ← Convert datetime to string
             }
-            await manager.send_personal_message(json.dumps(response_data),user_id)
+            # call the send_personal_message from websockets.py
+            await manager.send_personal_message(response_data,user_id)
             print("entered into message sender method directly")
+    # this except block it hit when the user closes the tab
+    # or the server stops or crashes
     except WebSocketDisconnect:
         manager.disconnect(user_id)
+    # this is hit if any exception in the code like any syntax or validation errors
     except Exception as e:
         print(e)
         manager.disconnect(user_id)
