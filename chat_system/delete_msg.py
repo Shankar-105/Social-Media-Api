9576bc526ec4fc9a7fc10796f35982a14ea21355
Shankar-105 <shankarbhavani862@gmail.com>
2025-11-09 21:27:14 +0530

    user can delete msg for him

--- chat_system/delete_msg.py
+++ chat_system/delete_msg.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect,Depends,Query
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect,Depends,Query,HTTPException
 from app import schemas, models, oauth2,db
 from sqlalchemy.orm import Session
 from app.my_utils.socket_manager import manager
@@ -7,10 +7,24 @@ from datetime import datetime
 
 router=APIRouter(tags=['delete msg'])
 
-@router.websocket("/delete-msg/{msg_id}")
+@router.post("/delete/for-me/{msg_id}")
 async def deleteForMe(
     msg_id: int,
     token: str=Query(None,description="Search query params"),
-    db: Session=Depends(db.getDb)
+    db: Session=Depends(db.getDb),
+    me: models.UniqueConstraint = Depends(oauth2.getCurrentUser),
 ):
-    None
+    message=db.query(models.Message).filter(
+        models.Message.id==msg_id,
+        models.Message.sender_id==me.id
+    )
+    if not message:
+        raise HTTPException(status_code=404,detail="Message not found")
+    deleted_msg=models.DeletedMessage(
+        user_id=me.id,
+        message_id=msg_id
+    )
+    db.add(deleted_msg)
+    db.commit()
+    db.refresh(deleted_msg)
+    return {"message_id": msg_id, "detail": "Deleted for you"}
